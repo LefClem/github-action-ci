58751b7f5f8ed67538e9c2471303f30a4a587e1c
LefClem <lefranc.clment7@gmail.com>
2024-03-12 10:06:53 +0100

    add button front

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <h1>Hello World !</h1>
         <img src={logo} className="App-logo" alt="logo" />
+        <button>Test</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
